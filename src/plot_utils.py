e2e884196b0f021032b6f63d77bc378bd0720f89
EtiNL <etienne.lanzeray@gmail.com>
2025-11-08 20:56:01 +0100

    cleaned code for analytic sim

---
+++ src/plot_utils.py
@@ -0,0 +1,34 @@
+import plotly.io as pio
+import plotly.graph_objects as go
+
+def plot_sim_result(x,y,z,notebook_plot = True):
+    if notebook_plot == False:
+        pio.renderers.default = "browser"
+
+    fig = go.Figure(data=[
+            go.Surface(
+                x=x,         # z-axis in formula
+                y=y,         # t-axis
+                z=z,
+                colorscale="Viridis",
+                opacity=0.9,
+                contours=dict(
+                    z=dict(
+                        usecolormap=True,
+                        highlight=False,
+                    )
+                ),
+            )
+        ])
+
+    fig.update_layout(
+        autosize=True,
+        margin=dict(l=0, r=0, b=0, t=0),
+        scene=dict(
+            xaxis_title="z",
+            yaxis_title="t",
+            zaxis_title="x(t)(z)",
+        )
+    )
+
+    fig.show()
